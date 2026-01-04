537eb3fcc3e11e1dc2afa3218c5a6dc9d9a9c4df
marv <marv.kopka@web.de>
2026-01-04 21:21:12 +0100

    first draft

---
+++ Gw2AddonManagement.Updater/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using CommunityToolkit.Mvvm.DependencyInjection;
+
+namespace Gw2AddonManagement.Updater
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Ioc.Default.ConfigureServices();
+        }
+    }
+}
---
+++ Gw2AddonManagement.Updater/MainWindowModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Gw2AddonManagement.Updater;
+
+public partial class MainWindowModel : ObservableObject
+{
+    [ObservableProperty]
+    private double _progress;
+
+    [RelayCommand]
+    private void Initialize()
+    {
+    }
+}
